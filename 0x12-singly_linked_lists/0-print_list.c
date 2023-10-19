e1d335fa1ec74a652a6bd58b87aa7e0740370ccf
BizNextStep <charmaine.n.mbatha@icloud.com>
2023-10-19 14:38:02 -0700

    Lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,29 +1,24 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
+ * print_list - prints all the elements of a list
+ * @h: pointer to the list
  *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
+	const list_t *cursor = h;
 	size_t count = 0;
-	const list_t *current = h;
 
-	while (current)
+	while (cursor != NULL)
 	{
-		if (current->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
+		if (cursor->str != NULL)
+			printf("[%d] %s\n", cursor->len, cursor->str);
 		else
-		{
-			printf("%s\n", current->str);
-		}
-
-		count++;
-		current = current->next;
+			printf("[0] (nil)\n");
+		count += 1;
+		cursor = cursor->next;
 	}
 
 	return (count);
