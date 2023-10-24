2418f86d28cce85e4fb56bdfeede4a3ec9d1f98f
BizNextStep <charmaine.n.mbatha@icloud.com>
2023-10-24 15:54:38 -0700

    updates

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -12,11 +11,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 	
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return;
 	
 	while (*head != NULL)
-	
 	{
 		temp = *head;
 		*head = (*head)->next;
