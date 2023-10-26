a6483b51b2acd0f9702018feff83c6e8d0003892
BizNextStep <charmaine.n.mbatha@icloud.com>
2023-10-26 10:54:05 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int val = 0;
 	int i = 0;
-	
+
 	if (b == NULL)
 		return 0;
-	
+
 	while (b[i] != '\0')
 	{
-	if (b[i] != '0' && b[i] != '1')
-		return 0;
+		if (b[i] != '0' && b[i] != '1')
+			return 0;
 
-	val <<= 1;
-	val += b[i] - '0';
-	i++;
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
 	}
 
 	return val;
