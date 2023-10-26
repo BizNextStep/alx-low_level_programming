49bb292d8d815a66289abd12b353a65fed8f9044
BizNextStep <charmaine.n.mbatha@icloud.com>
2023-10-26 16:15:48 -0700

    returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,31 +1,22 @@
-#include "main.h"
+#include"main.h"
 #include <stdio.h>
 
 /**
- * get_bit - gets the value of a bit at a given index
- * @n: unsigned long integer
- * @index: index of the bit to retrieve, starting from 0
+ * get_bit - prog returns the value of a bit at a given index.
+ * @n: number to check bits in
+ * @index: index at which to check bit
  *
- * Return: value of the bit at index or -1 if an error occurred
+ * Return: value of the bit, or -1 if there is an error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-	{
-		return (-1); /* Error: index out of range */
-	}
+	unsigned long int divisor, check;
 
-	return ((n >> index) & 1);
-}
-
-int main(void)
-
-{
-	unsigned long int number = 12345;
-	unsigned int bit_index = 3;
-	int bit_value = get_bit(number, bit_index);
-	printf("The value of bit %u in number %lu is %d\n",
-			bit_index, number, bit_value);
-
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	divisor = 1 << index;
+	check = n & divisor;
+	if (check == divisor)
+		return (1);
 	return (0);
 }
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to an unsigned long integer
+ * @index: index of the bit to set, starting from 0
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8) {
+		return -1;	/* Error: index out of range */
+	}
+
+	*n |= 1UL << index;
+
+	return 1;
+}
+
